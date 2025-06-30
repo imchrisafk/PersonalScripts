09d3cf43718941b855ed8d170a1f95b4e7452762
Chris <chrisafk@pm.me>
2025-06-30 14:02:30 -0400

    Made usage comments consistent across all bash scripts.

--- bash/squarizevideo.sh
+++ bash/squarizevideo.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-# Usage: squarizevideo.sh [input filename] [output filename]
+# Usage: ./squarizevideo.sh <input_file> <output_file>
 
 # Takes in a vertical video and converts it to a 1:1 aspect ratio video.
 # A zoomed and blurred copy of the original video stream is layered in the background
 # to fill the left and right margins. The output is encoded as an h264+aac mp4 file.
 
-ffmpeg -i "${1}" -filter_complex "[0:v]split=2[blur][vid];[blur]tblend,fps=60,boxblur=8,scale=in_h:-1,crop=in_w:in_w:0:in_h/3[bg];[vid]tblend,fps=60[ov];[bg][ov]overlay=(W-w)/2,fps=60" -c:v libx264 -c:a aac -b:v 10M -b:a 320k "${2}"
+# Configuration
+INPUT_FILE="$1"
+OUTPUT_FILE="$2"
+
+ffmpeg -i "$INPUT_FILE" -filter_complex "[0:v]split=2[blur][vid];[blur]tblend,fps=60,boxblur=8,scale=in_h:-1,crop=in_w:in_w:0:in_h/3[bg];[vid]tblend,fps=60[ov];[bg][ov]overlay=(W-w)/2,fps=60" -c:v libx264 -c:a aac -b:v 10M -b:a 320k "$OUTPUT_FILE"
