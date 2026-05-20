4959cf7f97294235cfdb459387a823400642e9ac
Chris <chrisafk@pm.me>
2026-05-19 21:08:16 -0400

    Move output file definition to top

--- bash/squarizevideo.sh
+++ bash/squarizevideo.sh
@@ -17,6 +17,18 @@ fi
 
 INPUT_FILE="$1"
 
+# Set output file path, ensuring it ends with '.mp4'
+if [ -n "$2" ]; then
+    if [[ "$2" == *.mp4 ]]; then
+        OUTPUT_FILE="$2"
+    else
+        echo "Error: Second argument must have .mp4 extension"
+        exit 1
+    fi
+else
+    OUTPUT_FILE="$(dirname "$INPUT_FILE")/$(basename "$INPUT_FILE" | cut -f 1 -d '.').squarized.mp4"
+fi
+
 # Get video dimensions
 WIDTH=$(ffprobe -v error -select_streams v:0 \
     -show_entries stream=width -of csv=p=0 "$INPUT_FILE")
@@ -81,18 +93,6 @@ crop=${S}:${S}:(iw-${S})/2:(ih-${S})/2,\
 boxblur=luma_radius=20:luma_power=2[bg];\
 [bg][fg]overlay=${OV_X}:${OV_Y}\
 "
-
-# Set output file path, ensuring it ends with '.mp4'
-if [ -n "$2" ]; then
-    if [[ "$2" == *.mp4 ]]; then
-        OUTPUT_FILE="$2"
-    else
-        echo "Error: Second argument must have .mp4 extension"
-        exit 1
-    fi
-else
-    OUTPUT_FILE="$(dirname "$INPUT_FILE")/$(basename "$INPUT_FILE" | cut -f 1 -d '.').squarized.mp4"
-fi
 
 # Process video: split stream, create blurred background by zooming and cropping input to match height as width, overlay original video, encode to H.264/AAC
 ffmpeg -i "$INPUT_FILE" -filter_complex "[0:v]split=2[blur][vid];[blur]tblend,fps=60,boxblur=8,scale=in_h:-1,crop=in_w:in_w:0:in_h/3[bg];[vid]tblend,fps=60[ov];[bg][ov]overlay=(W-w)/2,fps=60" -c:v libx264 -c:a aac -b:v 10M -b:a 320k "$OUTPUT_FILE"
